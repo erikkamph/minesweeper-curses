ed8e3054a4885e3fefb12aa2dfda1ac15d97a3d4
Erik Kamph <erikkamph38@gmail.com>
2019-11-10 16:20:30 +0100

    Something

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ class Game:
         self.board = self.create_board()
         self.score = 0
         self.nr_bombs = n + int(n/2)
+        self.bomb_locations = []
 
     def create_board(self):
         outer = [None] * self.high
@@ -129,13 +130,14 @@ class Game:
     def place_bomb(self):
         r = random.randint(0, self.high - 1)
         c = random.randint(0, self.high - 1)
-        if self.board[c][r].is_a_bomb():
+        if self.board[r][c].is_a_bomb():
             self.place_bomb()
         else:
-            self.board[c][r].set_is_bomb(True)
+            self.board[r][c].set_is_bomb(True)
+            self.bomb_locations.append((r, c))
 
     def reveal(self, x, y):
-        if not self.board[y][x].has_opened():
+        if not self.board[y][x].has_opened() and not (self.board[y][x].is_a_bomb() and self.board[y][x].is_flagged()):
             self.board[y][x].open(True)
             if not self.board[y][x].has_gotten_points:
                 self.score += self.board[y][x].neighbours
@@ -171,19 +173,6 @@ class Game:
                         and not self.board[y][x].is_a_bomb():
                     self.reveal(x, y)
 
-    def has_exploded(self):
-        for i in range(self.high - 1):
-            for c in self.board[i]:
-                if c.hasExploded:
-                    self.reveal_bombs()
-                    return True
-
-    def reveal_bombs(self):
-        for x in range(self.high - 1):
-            for y in range(self.high - 1):
-                if self.board[y][x].is_a_bomb():
-                    self.board[y][x].exploded()
-
     def win(self):
         i = 0
         for row in range(self.high - 1):
@@ -194,6 +183,14 @@ class Game:
             return True
         else:
             return False
+
+    def loose(self):
+        lost = False
+        for row in range(self.high):
+            for col in range(self.high):
+                if self.board[row][col].hasExploded:
+                    lost = True
+        return lost
 
 
 def do_something(x, y, c, g):
@@ -273,6 +270,8 @@ def welcome(stdscr):
     if re.search("[A-Za-z]+", c) or c is "":  # Test if c contains any character
         return 0
     else:  # When c only contains numbers we can send return it
+        if int(c) > int(rows):
+            return 0
         return int(c)
 
 
@@ -334,9 +333,15 @@ def main(stdscr):
         print_info(stdscr, g, c)
         if y not in ("n", "N"):
             if g.win():
+                print_info(stdscr, g, c)
+                stdscr.refresh()
                 stdscr.addstr(13, g.high + 2 + len("You won!"), "You won!",
                               curses.color_pair(5) + curses.A_BOLD + curses.A_BLINK)
-            elif g.has_exploded():
+            elif g.loose():
+                for r, c in g.bomb_locations:
+                    g.reveal(c, r)
+                print_info(stdscr, g, c)
+                stdscr.refresh()
                 stdscr.addstr(13, g.high + 2 + len("Game Over!"), "Game Over!", curses.A_BOLD + curses.color_pair(3))
             y = print_end(stdscr, length, g)
 
